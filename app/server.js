00b907c843b96d924c6be5724ddc4b58f977cae0
Arsen Pyuskyulyan <arspyus@gmail.com>
2021-11-02 20:01:18 +0400

    Added buildspecs.yml to .dockerignore

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 const app = express();
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Hello world from ${process.env.HOSTNAME} container!</h1>
+        <h1>!!!Hello world from ${process.env.HOSTNAME} container!</h1>
     `);
 });
 
